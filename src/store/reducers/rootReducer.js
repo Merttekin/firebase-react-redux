d6c4552d2d7ad2a21fc2208ffe1b5db43bb9615a
unknown <merttekin94@gmail.com>
2019-03-18 21:09:50 +0300

    add thunk

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,10 @@
 import authReducer from './authReducer';
 import projectReducer from './projectReducer';
-import { combineReducer } from 'redux';
+import { combineReducers } from 'redux';
 
-const rootReducer = combineReducer({
-    auth: authReducer,
-    project: projectReducer
-})
+const rootReducer = combineReducers({
+  auth: authReducer,
+  project: projectReducer
+});
 
 export default rootReducer;
