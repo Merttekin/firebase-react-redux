35605292ce4b425f5553310db611a3fff91f43db
unknown <merttekin94@gmail.com>
2019-03-19 22:16:38 +0300

    syncing data firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,17 @@
 export const createProject = (project) =>{
-    return(dispatch, getState) => {
-        dispatch({ type: 'CREATE_PROJECT', project })
+    return(dispatch, getState, { getFirabase, getFirestore }) => {
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Mert',
+            authorLastName: 'Tekin',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project })
+        }).catch((err) =>{
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+        });
+        
     }
 }
