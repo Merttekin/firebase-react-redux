0832f5377fc62179064bcdad5a530efa02b7e4ac
unknown <merttekin94@gmail.com>
2019-03-21 22:09:53 +0300

    26auth status

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,22 +4,22 @@ import SignedInLinks from './SıgnedInLinks';
 import SignedOutLinks from './SıgnedOutLinks';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+    const { auth } = props;
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
     return(
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link to="/" className="brand-logo">Firebase</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                { links }
             </div>
         </nav>
     )
 }
 
 const mapStateToProps = (state) => {
-    console.log(state);
     return{
-
+        auth: state.firebase.auth
     }
 }
 
